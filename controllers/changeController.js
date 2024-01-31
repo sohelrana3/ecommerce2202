42db021e947daa09547d4f4df827f77775e42e58
Sohel Rana <expartdesigns5@gmail.com>
2024-01-31 23:10:26 +0600

    changeconrtoller & login Controller

--- controllers/changeController.js
+++ controllers/changeController.js
@@ -1,8 +1,37 @@
+const User = require("../model/userSchema");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const passwordValidation = require("../helpers/passwordValidation");
+
 const changeController = (req, res) => {
     try {
         const { token, password } = req.body;
 
-        res.send(password);
+        jwt.verify(token, "123456", async function (err, decoded) {
+            console.log(decoded.email); // bar
+
+            // passwordValidation
+            if (password) {
+                if (!passwordValidation(password)) {
+                    return res.send({
+                        error: " Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character",
+                    });
+                }
+            }
+
+            bcrypt.hash(password, 10, async function (err, hash) {
+                let existingUser2 = await User.findOneAndUpdate(
+                    { email: decoded.email },
+                    { $set: { password: hash, token: "" } },
+                    {
+                        returnNewDocument: true,
+                        new: true,
+                        strict: false,
+                    }
+                );
+                res.send({success: "Password Update successfull"})
+            });
+        });
     } catch (error) {
         console.log(error);
     }
--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,31 +1,30 @@
-let User = require("../model/userSchema")
-const bcrypt = require('bcrypt');
+let User = require("../model/userSchema");
+const bcrypt = require("bcrypt");
 
-let loginController = async (req,res)=>{
-    let {email,password} = req.body
-    
-    console.log(email,password)
+let loginController = async (req, res) => {
+    let { email, password } = req.body;
 
-    let existingUser = await User.find({email:email})
+    console.log(email, password);
 
+    let existingUser = await User.find({ email: email });
 
-    if(existingUser.length == 0){
-        res.send({error:"Credencial is not valid"})
-    }else{
-        bcrypt.compare(password, existingUser[0].password, function(err, result) {
-            if(result){
-                console.log(existingUser[0]);
-                const { password: pass, ...data } = existingUser[0]._doc;
-                res.send(data)
-                // res.send({success:"Login Successful"})
-            }else{
-                res.send({error:"Credencial is not valid"})
+    if (existingUser.length == 0) {
+        res.send({ error: "Credencial is not valid" });
+    } else {
+        bcrypt.compare(
+            password,
+            existingUser[0].password,
+            function (err, result) {
+                if (result) {
+                    console.log(existingUser[0]);
+                    const { password, ...data } = existingUser[0]._doc;
+                    res.send(data);
+                } else {
+                    res.send({ error: "Credencial is not valid" });
+                }
             }
-        });
+        );
     }
-  
+};
 
-
-}
-
-module.exports = loginController
+module.exports = loginController;
